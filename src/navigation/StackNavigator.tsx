a426f30d86d0c8caffa7902b08d576c9b0393a46
Dayni20 <dayniliss@hotmail.com>
2026-05-09 00:20:18 -0500

    feat: add stack navigation types and navigator

---
+++ src/navigation/StackNavigator.tsx
@@ -0,0 +1,40 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { COLORS } from "../styles/appStyles";
+import { ListScreen } from "../screens/ListScreen";
+import { RootStackParamList } from "./typesNavigation";
+import DetailScreen from '../screens/DetailScreen';
+import { FormScreen } from '../screens/FormScreen';
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="List"
+      screenOptions={{
+        headerStyle: { backgroundColor: COLORS.headerBg },
+        headerTintColor: COLORS.white,
+        headerTitleStyle: { fontWeight: "bold" },
+      }}
+    >
+      <Stack.Screen
+        name="List"
+        component={ListScreen}
+        options={{ title: "My Gadgets" }}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={DetailScreen}
+        options={{ title: "Gadget Details" }}
+      />
+      <Stack.Screen
+        name="Form"
+        component={FormScreen}
+        options={({ route }) => ({
+          title: route.params?.id ? "Edit Gadget" : "New Gadget",
+          headerRight: () => null,
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
